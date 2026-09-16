cdecaa742c7d4a2a325669a682f188a8f9b78d66
linn308 <linhh1188@gmail.com>
2026-09-16 13:37:10 +0700

    sync external link

--- js/data-3d.js
+++ js/data-3d.js
@@ -68,7 +68,7 @@ const PROJECTS_3D = {
     test: {
     title: 'Local Brands - Magazine Layout',
     year: '2021',
-    cover: { type: 'model', src: '../../source-files/3d/glb/Kord_lo_01.glb' },
+    cover: { type: 'model', src: '../../source-files/3d/pixivore/02.glb' },
     category: { vi: 'Editorial', en: 'Editorial' },
     subtitle: { vi: 'Editorial Design', en: 'Editorial Design' },
     desc: {
